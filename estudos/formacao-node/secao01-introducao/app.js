210743d9eea4aaaf395c9199725ce8af1c31509a
Alessandro Lima <alessandro.lima@msn.com>
2020-09-15 23:54:01 -0300

    Formação node - section01: Introdução ao node.js

---
+++ estudos/formacao-node/secao01-introducao/app.js
@@ -0,0 +1,8 @@
+const calculadora =  require("./calculadora");
+
+console.log(calculadora.soma(20,50));
+console.log(calculadora.sub(80,50)); 
+console.log(calculadora.mult(30,50)); 
+console.log(calculadora.div(100,50)); 
+console.log(calculadora.nome); 
+
